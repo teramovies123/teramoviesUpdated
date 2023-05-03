dda7ef64e8c150b0365e32910bb9e4da9d01a240
teramovies123 <132097741+teramovies123@users.noreply.github.com>
2023-05-03 22:36:58 +0530

    Add files via upload

---
+++ wbs/scr.js
@@ -0,0 +1,10 @@
+
+            atOptions = {
+                'key' : '7be2827fd05b6b05d01b5a62f24f2b17',
+                'format' : 'iframe',
+                'height' : 250,
+                'width' : 300,
+                'params' : {}
+            };
+            document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/7be2827fd05b6b05d01b5a62f24f2b17/invoke.js"></scr' + 'ipt>');
+        
