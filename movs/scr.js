d4b532f8e91c49ff1c6b949dba2f2f5fa9cb87fc
teramovies123 <132097741+teramovies123@users.noreply.github.com>
2023-05-08 06:21:02 +0530

    Add files via upload

--- movs/scr.js
+++ movs/scr.js
@@ -7,3 +7,5 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://protectorparsleybrisk.com/70bcb9cf42933724427eeac1fe70568c/invoke.js"></scr' + 'ipt>');
+	(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})
+	(document.createElement('script'),'https://inklinkor.com/tag.min.js',5932776,document.body||document.documentElement)
