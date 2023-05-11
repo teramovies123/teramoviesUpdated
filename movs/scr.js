718a27d704df9f4cc6a092a3de03ceec790f0608
teramovies123 <132097741+teramovies123@users.noreply.github.com>
2023-05-11 06:09:10 +0530

    Add files via upload

--- movs/scr.js
+++ movs/scr.js
@@ -6,4 +6,6 @@
 		'width' : 468,
 		'params' : {}
 	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://protectorparsleybrisk.com/70bcb9cf42933724427eeac1fe70568c/invoke.js"></scr' + 'ipt>');
+
 	
